9d9b08a9b4bcab62d35398c241776c08de3b8992
Zhiwen Hu <jackhu.521.rose@gmail.com>
2024-02-16 18:38:21 +0800

    ch2

--- src/kernel/lib/method.h
+++ src/kernel/lib/method.h
@@ -11,3 +11,16 @@ void Panic(const char *fmt, ...);
 void AssertFailed(char *exp, char *file, char *base, i32 line);
 #define Assert(exp) \
     if (!(exp)) AssertFailed(#exp, __FILE__, __BASE_FILE__, __LINE__)
+
+// log
+void DebugHandle(const char *file, i32 line, const char *fmt, ...);
+void TraceHandle(const char *file, i32 line, const char *fmt, ...);
+void InfoHandle(const char *file, i32 line, const char *fmt, ...);
+void WarnHandle(const char *file, i32 line, const char *fmt, ...);
+void ErrorHandle(const char *file, i32 line, const char *fmt, ...);
+
+#define Debug(fmt, ...) DebugHandle(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define Trace(fmt, ...) TraceHandle(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define Info(fmt, ...) InfoHandle(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define Warn(fmt, ...) WarnHandle(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define Error(fmt, ...) ErrorHandle(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
