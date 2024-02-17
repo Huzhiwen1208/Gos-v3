98abfec26515be947dc186b52a06ee37dd18bb0d
Zhiwen Hu <jackhu.521.rose@gmail.com>
2024-02-17 12:14:23 +0800

    fix log.c

---
+++ src/kernel/lib/log.c
@@ -0,0 +1,21 @@
+#include "mod.h"
+
+void DebugHandle(const char *file, i32 line, const char *fmt, ...) {
+    Panic("not implemented");
+}
+
+void TraceHandle(const char *file, i32 line, const char *fmt, ...) {
+    Panic("not implemented");
+}
+
+void InfoHandle(const char *file, i32 line, const char *fmt, ...) {
+    Panic("not implemented");
+}
+
+void WarnHandle(const char *file, i32 line, const char *fmt, ...) {
+    Panic("not implemented");
+}
+
+void ErrorHandle(const char *file, i32 line, const char *fmt, ...) {
+    Panic("not implemented");
+}
