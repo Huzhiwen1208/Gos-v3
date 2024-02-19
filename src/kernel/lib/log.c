367ca1355b48cc181f36bdd380465f05215e5285
Zhiwen Hu <jackhu.521.rose@gmail.com>
2024-02-19 11:10:40 +0800

    init ch3

--- src/kernel/lib/log.c
+++ src/kernel/lib/log.c
@@ -1,21 +1,46 @@
 #include "mod.h"
 
-void DebugHandle(const char *file, i32 line, const char *fmt, ...){
-    Panic("DebugHandle not implemented");
+void DebugHandle(const char *file, i32 line, const char *fmt, ...) {
+    char buf[1024];
+    va_list args;
+    va_start(args, fmt);
+    Vsprintf(buf, fmt, args);
+    ConsoleAlignLine();
+    PrintWithColor(CYAN, "[DEBUG] %s:%d %s\n", file, line, buf);
 }
 
-void TraceHandle(const char *file, i32 line, const char *fmt, ...){
-    Panic("TraceHandle not implemented");
+void TraceHandle(const char *file, i32 line, const char *fmt, ...) {
+    char buf[1024];
+    va_list args;
+    va_start(args, fmt);
+    Vsprintf(buf, fmt, args);
+    ConsoleAlignLine();
+    PrintWithColor(GRAY, "[TRACE] %s:%d %s\n", file, line, buf);
 }
 
-void InfoHandle(const char *file, i32 line, const char *fmt, ...){
-    Panic("InfoHandle not implemented");
+void InfoHandle(const char *file, i32 line, const char *fmt, ...) {
+    char buf[1024];
+    va_list args;
+    va_start(args, fmt);
+    Vsprintf(buf, fmt, args);
+    ConsoleAlignLine();
+    PrintWithColor(GREEN, "[INFO] %s:%d %s\n", file, line, buf);
 }
 
-void WarnHandle(const char *file, i32 line, const char *fmt, ...){
-    Panic("WarnHandle not implemented");
+void WarnHandle(const char *file, i32 line, const char *fmt, ...) {
+    char buf[1024];
+    va_list args;
+    va_start(args, fmt);
+    Vsprintf(buf, fmt, args);
+    ConsoleAlignLine();
+    PrintWithColor(YELLOW, "[WARN] %s:%d %s\n", file, line, buf);
 }
 
-void ErrorHandle(const char *file, i32 line, const char *fmt, ...){
-    Panic("ErrorHandle not implemented");
+void ErrorHandle(const char *file, i32 line, const char *fmt, ...) {
+    char buf[1024];
+    va_list args;
+    va_start(args, fmt);
+    Vsprintf(buf, fmt, args);
+    ConsoleAlignLine();
+    PrintWithColor(RED, "[ERROR] %s:%d %s\n", file, line, buf);
 }
--- src/kernel/memory/stable.c
+++
@@ -1,9 +0,0 @@
-#include "mod.h"
-
-PhysicalAddress StableMalloc(Size size) {
-    Panic("not implemented");
-}
-
-void StableFree(PhysicalAddress address){
-    Panic("not implemented");
-}
