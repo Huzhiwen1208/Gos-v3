4d71186c7405b593b77a890f6db91046b14e3e42
jackhu <jackhu.521.rose@gmail.com>
2024-12-15 13:49:49 +0800

    add some log

--- src/kernel/memory_allocator_test.c
+++ src/kernel/memory_allocator_test.c
@@ -1,63 +1,71 @@
 #include "memory/mod.h"
+#include "lib/mod.h"
+#include "console/mod.h"
 
+/// @brief 测试BuddyAllocator
 void testBuddyAllocator() {
-    char* p1 = (char*)Malloc(100);
-    char* p2 = (char*)Malloc(100);
+    PrintWithColor(GREEN, ">>>>>>>>>>> Test BuddyAllocator Start <<<<<<<<<<<\n");
+
+    char *p1 = (char *)Malloc(100);
+    char *p2 = (char *)Malloc(100);
     *p1 = 'a';
-    *(p1+1) = 'b';
-    *(p1+2) = 'c';
-    *(p1+3) = '\0';
+    *(p1 + 1) = 'b';
+    *(p1 + 2) = 'c';
+    *(p1 + 3) = '\0';
 
     *p2 = 'd';
-    *(p2+1) = 'e';
-    *(p2+2) = 'f';
-    *(p2+3) = 'g';
-    *(p2+4) = '\0';
+    *(p2 + 1) = 'e';
+    *(p2 + 2) = 'f';
+    *(p2 + 3) = 'g';
+    *(p2 + 4) = '\0';
 
     Printf("p1=%s, p2=%s\n", p1, p2);
 
     Free(p1);
 
-    char* p3 = (char*)Malloc(100);
+    char *p3 = (char *)Malloc(100);
     *p3 = 'h';
-    *(p3+1) = 'i';
-    *(p3+2) = 'j';
-    *(p3+3) = '\0';
+    *(p3 + 1) = 'i';
+    *(p3 + 2) = 'j';
+    *(p3 + 3) = '\0';
 
     Printf("p3=%s\n", p3);
 
     Free(p3);
     Free(p2);
+    PrintWithColor(GREEN, ">>>>>>>>>>> Test BuddyAllocator PASSED <<<<<<<<<<\n");
 }
 
+/// @brief 测试StableAllocator
 void testStableAllocator() {
-    char* p1 = (char*)StableMalloc(100);
-    char* p2 = (char*)StableMalloc(100);
+    PrintWithColor(GREEN, ">>>>>>>>>>> Test StableAllocator Start <<<<<<<<<<<\n");
+    char *p1 = (char *)StableMalloc(100);
+    char *p2 = (char *)StableMalloc(100);
     *p1 = 'a';
-    *(p1+1) = 'b';
-    *(p1+2) = 'c';
-    *(p1+3) = '\0';
+    *(p1 + 1) = 'b';
+    *(p1 + 2) = 'c';
+    *(p1 + 3) = '\0';
 
     *p2 = 'd';
-    *(p2+1) = 'e';
-    *(p2+2) = 'f';
-    *(p2+3) = 'g';
-    *(p2+4) = '\0';
+    *(p2 + 1) = 'e';
+    *(p2 + 2) = 'f';
+    *(p2 + 3) = 'g';
+    *(p2 + 4) = '\0';
 
     Printf("p1=%s, p2=%s\n", p1, p2);
 
     StableFree(p1);
 
-    char* p3 = (char*)StableMalloc(100);
+    char *p3 = (char *)StableMalloc(100);
     *p3 = 'h';
-    *(p3+1) = 'i';
-    *(p3+2) = 'j';
-    *(p3+3) = '\0';
+    *(p3 + 1) = 'i';
+    *(p3 + 2) = 'j';
+    *(p3 + 3) = '\0';
 
     Printf("p3=%s\n", p3);
 
     StableFree(p3);
     StableFree(p2);
 
-    Printf("StableAllocator test passed\n");
+    PrintWithColor(GREEN, ">>>>>>>>>>> Test StableAllocator PASSED <<<<<<<<<<\n");
 }
