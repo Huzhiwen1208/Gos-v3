c97c6150cec965d8620c8496d9437fd78eeb3202
jackhu <jackhu.521.rose@gmail.com>
2024-12-15 13:35:19 +0800

    fix code style

--- src/kernel/memory/stable.c
+++ src/kernel/memory/stable.c
@@ -7,14 +7,11 @@ static i32 getFreeBlockIndex();                // 获取空闲的固定分区块
 static void freeBlock(i32 index);              // 释放固定分区块
 static StableBlock *getStableBlock(i32 index); // 根据索引获取相应的分区块的指针
 
-PhysicalAddress StableMalloc(Size size)
-{
+PhysicalAddress StableMalloc(Size size) {
     i32 pre = 0;
     i32 p = getStableBlock(0)->Next;
-    while (p != -1)
-    {
-        if (getStableBlock(p)->IsUsed == FALSE && getStableBlock(p)->BlockSize >= size)
-        {
+    while (p != -1) {
+        if (getStableBlock(p)->IsUsed == FALSE && getStableBlock(p)->BlockSize >= size) {
             break;
         }
         pre = p;
@@ -28,14 +25,11 @@ PhysicalAddress StableMalloc(Size size)
     return getStableBlock(p)->BaseAddress; // 找到了满足条件的分区块，将其标记为已使用，并返回其起始地址
 }
 
-void StableFree(PhysicalAddress address)
-{
+void StableFree(PhysicalAddress address) {
     i32 pre = 0;
     i32 p = getStableBlock(0)->Next; // 从第一块遍历，直到找到具有指定物理地址的分区块。
-    while (p != -1)
-    {
-        if (getStableBlock(p)->BaseAddress == address)
-        {
+    while (p != -1) {
+        if (getStableBlock(p)->BaseAddress == address) {
             break;
         }
         pre = p;
@@ -44,10 +38,8 @@ void StableFree(PhysicalAddress address)
     getStableBlock(p)->IsUsed = FALSE; // 标为未使用
 }
 
-void globalStableAllocatorInit()
-{
-    for (Size i = 0; i < StablePages; i++)
-    {
+void globalStableAllocatorInit() {
+    for (Size i = 0; i < StablePages; i++) {
         globalStableAllocator.Blocks[i].BaseAddress = StartAddress + i * StableBlockSize; // 起始地址加上该分区块在稳定分配器中的索引乘以分区块大小，确保每个分区块的起始地址都不同。
         globalStableAllocator.Blocks[i].BlockSize = StableBlockSize;                      // 固定的分区块大小
         globalStableAllocator.Blocks[i].IsUsed = FALSE;                                   // 分区块未被使用
@@ -61,12 +53,9 @@ void globalStableAllocatorInit()
     globalStableAllocator.Blocks[StablePages - 1].Next = -1; // 将最后一个分区块的 Next 设置为 -1，表示链表的末尾
 }
 
-static i32 getFreeBlockIndex()
-{
-    for (i32 i = 0; i < StablePages; i++)
-    {
-        if (globalStableAllocator.Blocks[i].IsUsed == FALSE)
-        {
+static i32 getFreeBlockIndex() {
+    for (i32 i = 0; i < StablePages; i++) {
+        if (globalStableAllocator.Blocks[i].IsUsed == FALSE) {
             return i;
         }
     }
@@ -75,7 +64,6 @@ static i32 getFreeBlockIndex()
 }
 
 // 遍历空闲分区块，找到一个未被使用的分区块，并返回其索引。如果没有找到未被使用的分区块，触发一个错误。
-static StableBlock *getStableBlock(i32 index)
-{
+static StableBlock *getStableBlock(i32 index) {
     return &globalStableAllocator.Blocks[index]; //& 操作符取得了该分区块的地址
 } // 根据索引获取固定分区块的指针，并返回该指针。
