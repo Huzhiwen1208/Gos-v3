f9f49077ebf14b74af9482aa15e68caf66557f12
jackhu <jackhu.521.rose@gmail.com>
2024-12-17 00:30:16 +0800

    add some comment

--- src/kernel/common/type.h
+++ src/kernel/common/type.h
@@ -14,14 +14,14 @@ typedef char i8;
 typedef short i16;
 typedef int i32;
 typedef long long i64;
-typedef char* String;
+typedef char *String;
 typedef unsigned int Size;
 
 typedef unsigned int PhysicalAddress;
 typedef unsigned int PhysicalPageNumber;
 
 // 可变参数列表
-typedef char* va_list;
+typedef char *va_list;
 
 #define StructNotAlign __attribute__((packed));
 
@@ -30,3 +30,6 @@ typedef enum MachineMode {
     KernelMode,
     UserMode
 } MachineMode;
+
+/// @brief 使结构体不按边界对齐方式排布
+#define StructNotAlign __attribute__((packed));
