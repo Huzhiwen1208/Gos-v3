36e3e206f9d0d46b7e86a59a7511cd234f4db98f
jackhu <jackhu.521.rose@gmail.com>
2026-09-03 16:05:55 +0800

    fix mac

--- src/kernel/kernel_main.c
+++ src/kernel/kernel_main.c
@@ -1,9 +1,13 @@
 #include "common/mod.h"
-#include "lib/mod.h"
-#include "console/mod.h"
-#include "memory/mod.h"
-#include "int/mod.h"
-#include "process/mod.h"
+#include "console/type.h"
+#include "console/method.h"
+#include "lib/method.h"
+#include "memory/type.h"
+#include "memory/method.h"
+#include "int/type.h"
+#include "int/method.h"
+#include "process/type.h"
+#include "process/method.h"
 
 void testProcess() {
     Printf("Hello, World! This is a test process\n");
